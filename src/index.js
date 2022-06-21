7d79f3304fce53c178a01d40ef0b80a3a60c0e54
Josh Burd <Jburdt@gmail.com>
2022-06-21 13:55:26 -0400

    make task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("#create-task-form") 
+    
+  form.addEventListener('submit', makeTask) 
+    
+    function makeTask(e) {
+      e.preventDefault()
+      
+      let newtaskDescriptioin = document.querySelector("#new-task-description")
+      
+      let newTask = document.createElement('li')
+      
+      newTask.innerText = newtaskDescriptioin.value
+      appendTask(newTask)
+    }
+      function appendTask(task) {
+        document.querySelector('#tasks').appendChild(task)
+      }
 });
